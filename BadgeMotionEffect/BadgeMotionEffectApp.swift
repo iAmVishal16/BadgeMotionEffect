7981b13449dacd88e3a66ffa5fdedb0448705d13
Vishal Paliwal <vishal.paliwal@ril.com>
2023-08-23 11:33:50 +0530

    Initial Commit

---
+++ BadgeMotionEffect/BadgeMotionEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BadgeMotionEffectApp.swift
+//  BadgeMotionEffect
+//
+//  Created by Vishal Paliwal on 23/08/23.
+//
+
+import SwiftUI
+
+@main
+struct BadgeMotionEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
